01b1b88599fdad5d7ab47ec49f544a5d5ca05e4f
hihumikan <musecharo@gmail.com>
2023-01-28 17:12:02 +0900

    typo: /register

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
 
                 <div class="mt-8 flex flex-wrap justify-center gap-4">
                     <a class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                        href="/get-started">
+                        href="/register">
                         Get Started
                     </a>
 
@@ -118,9 +118,11 @@
                     </div>
                 </div>
             </div>
-            <button
-                class="flex mx-auto mt-16 text-white bg-blue-600 border-0 py-2 px-8 font-medium focus:outline-none hover:bg-blue-600 rounded text-lg">Get
-                Started</button>
+            <a href="/register">
+                <button
+                    class="flex mx-auto mt-16 text-white bg-blue-600 border-0 py-2 px-8 font-medium focus:outline-none hover:bg-blue-600 rounded text-lg">Get
+                    Started</button>
+            </a>
 
         </div>
     </section>
