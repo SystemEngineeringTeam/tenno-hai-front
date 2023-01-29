7f64c2668c6684562bafff6bdfed047ca141044f
hihumikan <musecharo@gmail.com>
2023-01-29 19:26:42 +0900

    feat: tutorialのステップ記載

--- laravel/resources/views/tutorial.blade.php
+++ laravel/resources/views/tutorial.blade.php
@@ -6,24 +6,122 @@
                     <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Tutorial</h1>
                     <div class="h-1 w-20 bg-blue-600 rounded"></div>
                 </div>
-                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">このページではインフラを触る為の必要な知識を学ぶことが出来ます。
-                    色々なセクションを通して、インフラの仕組みを理解し、触れるようになりましょう！
-                </p>
+                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">このページではインフラを触る為の必要な環境構築を学ぶことが出来ます。
+                    色々なセクションを通して、インフラの環境構築が出来るようになりましょう！</p>
             </div>
-            <div class="flex flex-wrap -m-4">
+
+        </div>
+    </section>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 mx-auto">
+            <div class="flex">
+                <div class="">
+                    <div class="flex relative pb-12">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 1</h2>
+                            <p class="leading-relaxed">Dockerのインストール</p>
+
+                        </div>
+                        <a href="/" class="absolute left-0 w-full h-0 bg-black invisible visible">
+                        </a>
+                    </div>
+                    <div class="flex relative pb-12">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 2</h2>
+                            <p class="leading-relaxed">Vice migas literally kitsch +1 pok pok. Truffaut hot chicken
+                                slow-carb health goth, vape typewriter.</p>
+                        </div>
+                    </div>
+                    <div class="flex relative pb-12">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <circle cx="12" cy="5" r="3"></circle>
+                                <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 3</h2>
+                            <p class="leading-relaxed">Coloring book nar whal glossier master cleanse umami. Salvia +1
+                                master cleanse blog taiyaki.</p>
+                        </div>
+                    </div>
+                    <div class="flex relative pb-12">
+                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
+                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                        </div>
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 4</h2>
+                            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                                bespoke try-hard cliche palo santo offal.</p>
+                        </div>
+                    </div>
+                    <div class="flex relative">
+                        <div
+                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                <path d="M22 4L12 14.01l-3-3"></path>
+                            </svg>
+                        </div>
+                        <div class="flex-grow pl-4">
+                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
+                            <p class="leading-relaxed">Pitchfork ugh tattooed scenester echo park gastropub whatever
+                                cold-pressed retro.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap mt-4">
                 <div class="xl:w-1/4 md:w-1/2 p-4">
                     <a href="/dashboard">
                         <div class="bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-gray-600 hover:shadow-md">
                             <img class="h-40 rounded w-full object-cover object-center mb-6"
                                 src="https://dummyimage.com/720x400" alt="content">
-                            <h3 class="tracking-widest text-blue-600 text-xs title-font font-extrabold">基礎知識</h3>
-                            <h2 class="text-lg text-gray-900 font-bold title-font mb-4">Dockerのインストール</h2>
-                            <p class="leading-relaxed text-base">天皇杯のタスクを触れるようにする為の基礎知識として、Dockerのインストールを行います。</p>
+                            <h3 class="tracking-widest text-blue-600 text-xs title-font font-extrabold">general</h3>
+                            <h2 class="text-lg text-gray-900 font-bold title-font mb-4">ssh設定</h2>
+                            <p class="leading-relaxed text-base">ホストマシンからDocker上のUbuntuにsshの設定をしてみましょう。</p>
                         </div>
                     </a>
                 </div>
             </div>
-
         </div>
     </section>
 </x-app-layout>
