e490b76edb1888f3729aa396fd758e476827d9fb
hihumikan <musecharo@gmail.com>
2023-01-29 18:45:00 +0900

    feat: tutorialの画面テンプレートの生成

--- laravel/resources/views/tutorial.blade.php
+++ laravel/resources/views/tutorial.blade.php
@@ -1,2 +1,29 @@
 <x-app-layout>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap w-full mb-20">
+                <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Tutorial</h1>
+                    <div class="h-1 w-20 bg-blue-600 rounded"></div>
+                </div>
+                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">このページではインフラを触る為の必要な知識を学ぶことが出来ます。
+                    色々なセクションを通して、インフラの仕組みを理解し、触れるようになりましょう！
+                </p>
+            </div>
+            <div class="flex flex-wrap -m-4">
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="/dashboard">
+                        <div class="bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-gray-600 hover:shadow-md">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6"
+                                src="https://dummyimage.com/720x400" alt="content">
+                            <h3 class="tracking-widest text-blue-600 text-xs font-medium title-font">基礎知識</h3>
+                            <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Dockerのインストール</h2>
+                            <p class="leading-relaxed text-base">天皇杯のタスクを触れるようにする為の基礎知識として、Dockerのインストールを行います。</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </x-app-layout>
