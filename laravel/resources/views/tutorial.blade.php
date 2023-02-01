c87c296d73c269db07724c8cc700f4a7aac4a066
hihumikan <musecharo@gmail.com>
2023-02-02 00:45:03 +0900

    doc: githubのreadmeを参照するようにした

--- laravel/resources/views/tutorial.blade.php
+++ laravel/resources/views/tutorial.blade.php
@@ -48,14 +48,10 @@
                         </div>
                         <div class="flex-grow pl-4">
                             <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 2</h2>
-                            <p class="leading-relaxed">Vice migas literally kitsch +1 pok pok. Truffaut hot chicken
-                                slow-carb health goth, vape typewriter.</p>
+                            <p class="leading-relaxed">天皇杯のプロジェクトのクローン</p>
                         </div>
                     </div>
                     <div class="flex relative pb-12">
-                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                        </div>
                         <div
                             class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -66,43 +62,10 @@
                         </div>
                         <div class="flex-grow pl-4">
                             <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 3</h2>
-                            <p class="leading-relaxed">Coloring book nar whal glossier master cleanse umami. Salvia +1
-                                master cleanse blog taiyaki.</p>
+                            <p class="leading-relaxed">Dockerの起動</p>
                         </div>
                     </div>
-                    <div class="flex relative pb-12">
-                        <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-                        </div>
-                        <div
-                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"></circle>
-                            </svg>
-                        </div>
-                        <div class="flex-grow pl-4">
-                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 4</h2>
-                            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
-                                bespoke try-hard cliche palo santo offal.</p>
-                        </div>
-                    </div>
-                    <div class="flex relative">
-                        <div
-                            class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                <path d="M22 4L12 14.01l-3-3"></path>
-                            </svg>
-                        </div>
-                        <div class="flex-grow pl-4">
-                            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
-                            <p class="leading-relaxed">Pitchfork ugh tattooed scenester echo park gastropub whatever
-                                cold-pressed retro.</p>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </div>
@@ -111,13 +74,32 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap mt-4">
                 <div class="xl:w-1/4 md:w-1/2 p-4">
-                    <a href="/dashboard">
+                    <a href="https://github.com/SystemEngineeringTeam/set-tenno-hai#getting-started">
                         <div class="bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-gray-600 hover:shadow-md">
                             <img class="h-40 rounded w-full object-cover object-center mb-6"
-                                src="https://dummyimage.com/720x400" alt="content">
-                            <h3 class="tracking-widest text-blue-600 text-xs title-font font-extrabold">general</h3>
-                            <h2 class="text-lg text-gray-900 font-bold title-font mb-4">ssh設定</h2>
-                            <p class="leading-relaxed text-base">ホストマシンからDocker上のUbuntuにsshの設定をしてみましょう。</p>
+                                src="https://dummyimage.com/720x400/5286ff/ffffff&text=1" alt="content">
+                            <h2 class="text-lg text-gray-900 font-bold title-font mb-4">Dockerのインストール</h2>
+                            <p class="leading-relaxed text-base">Dockerのインストール方法はこちらから</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="https://github.com/SystemEngineeringTeam/set-tenno-hai">
+                        <div class="bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-gray-600 hover:shadow-md">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6"
+                                src="https://dummyimage.com/720x400/5286ff/ffffff&text=2" alt="content">
+                            <h2 class="text-lg text-gray-900 font-bold title-font mb-4">天皇杯のプロジェクトのクローン</h2>
+                            <p class="leading-relaxed text-base">クローン方法はこちらから</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="https://github.com/SystemEngineeringTeam/set-tenno-hai#getting-started">
+                        <div class="bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-gray-600 hover:shadow-md">
+                            <img class="h-40 rounded w-full object-cover object-center mb-6"
+                                src="https://dummyimage.com/720x400/5286ff/ffffff&text=3" alt="content">
+                            <h2 class="text-lg text-gray-900 font-bold title-font mb-4">Dockerの起動</h2>
+                            <p class="leading-relaxed text-base">Dockerの起動方法はこちらから</p>
                         </div>
                     </a>
                 </div>
